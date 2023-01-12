b3330418a1727e7a0088ce53b179bd93019d2dd1
Your Name~ <maldolt@nv.ccsd.net>
2023-01-11 21:41:03 -0800

    Final draft

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,57 @@
 // TODO: Declare any global variables we need
+let numberHeads = 0
+let numberTails = 0
+let pctHeads = 0
+let pctTails = 0
+let total = 0
 
+//DOM variable
+const flipButton = document.querySelector("#flip")
+const clearButton = document.querySelector("#clear")
+const statusMessage = document.querySelector(".status")
+const image = document.querySelector("img")
+const numberHeadsCell = document.querySelector("#heads")
+const numberTailsCell = document.querySelector("#tails")
+const pctHeadsCell = document.querySelector("#heads-percent")
+const pctTailsCell = document.querySelector("#tails-percent")
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
 
     // Flip Button Click Handler
+    flipButton.addEventListener("click", function(e){
+        console.log("I've been clicked")
+        total ++ 
+        let result = Math.round(Math.random() *101)
+        console.log("result:",result)
+
+        //heads: 0-50
+        if (result < 51){
+            numberHeads ++
+            statusMessage.textContent = "You flipped heads!"
+            image.src = "./assets/images/penny-heads.jpg"
+        } 
+        //tails: 51-100
+        else {
+            numberTails ++
+            statusMessage.textContent = "You flipped tails!"
+            image.src = "./assets/images/penny-tails.jpg"
+        }
+        console.log(numberHeads, numberTails)
+
+        //calculate the percentages
+        pctHeads = Math.round(numberHeads/total *100)
+        pctTails = Math.round(numberTails/total *100)
+        console.log("percentages", pctHeads, pctTails)
+
+        //show the results in the scoreboard
+        numberHeadsCell.textContent = `${numberHeads}`
+        numberTailsCell.textContent = `${numberTails}`
+        pctTailsCell.textContent = `${pctTails}%`
+        pctHeadsCell.textContent = `${pctHeads}%`
+        
+    })
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
 
@@ -21,6 +64,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     // Clear Button Click Handler
+    clearButton.addEventListener("click", function(e){
+        total = 0
+        pctHeads = 0
+        pctTails = 0
+        numberHeads = 0
+        numberTails = 0
+
+        numberHeadsCell.textContent = `${numberHeads}`
+        numberTailsCell.textContent = `${numberTails}`
+        pctTailsCell.textContent = `${pctTails}%`
+        pctHeadsCell.textContent = `${pctHeads}%`
+
+        statusMessage.textContent = "Let's Get Rolling"
+        image.src = "./assets/images/penny-heads.jpg"
+    })
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
